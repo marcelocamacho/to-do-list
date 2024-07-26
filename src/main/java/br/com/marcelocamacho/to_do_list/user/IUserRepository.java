2e8bb740428fc96b61f536ca250cd9372e832e09
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2024-07-26 11:10:39 -0300

    Implementa o padrão repository usando o JPA para a entidade User

---
+++ src/main/java/br/com/marcelocamacho/to_do_list/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.marcelocamacho.to_do_list.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    
+}
--- src/main/java/br/com/marcelocamacho/to_do_list/user/UserController.java
+++ src/main/java/br/com/marcelocamacho/to_do_list/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.marcelocamacho.to_do_list.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
     public String create( @RequestBody UserModel userModel ){
         System.out.println(userModel.getName());
-        return userModel.toString();
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated.toString();
     }
 }
