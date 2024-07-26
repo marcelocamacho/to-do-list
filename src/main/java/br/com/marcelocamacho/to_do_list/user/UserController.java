76e9f11360e719b2c7b21b8c8bbd8dc3e2b69867
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2024-07-26 11:39:24 -0300

    Implementa o salvamento de senha criptografada utilizando o Bcrypt

--- src/main/java/br/com/marcelocamacho/to_do_list/user/UserController.java
+++ src/main/java/br/com/marcelocamacho/to_do_list/user/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.transaction.Status;
 
 @RestController
@@ -27,6 +28,11 @@ public class UserController {
             System.out.println("Usuário já existe;");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().
+            hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
 
