fbfa5180c42b26b8eab0da2b441aa3a4613d6ec0
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2024-07-27 19:51:50 -0300

    Implementa o Cadastro de Tarefas

---
+++ src/main/java/br/com/marcelocamacho/to_do_list/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.marcelocamacho.to_do_list.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+
+    
+}
